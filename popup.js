3dcc4b4309c37bf499947f4700c546666ac8743d
xpall <127076043+xpall@users.noreply.github.com>
2024-06-11 17:12:08 +0800

    Fixed html popup design

--- popup.js
+++ popup.js
@@ -16,6 +16,19 @@ document.addEventListener('DOMContentLoaded', function() {
         }).catch(function(err) {
           console.error("Error copying content to clipboard:", err);
         });
+        // Insert details to HTML
+        const insertJobTitle = document.getElementById("jobTitle");
+        const insertHours = document.getElementById('hours');
+        const insertCompensation = document.getElementById('compensation');
+        const insertContactPerson = document.getElementById('contactPerson');
+        const insertLink = document.getElementById('link');
+
+        insertJobTitle.innerText = data.job_title;
+        insertHours.innerText = data.hours_per_week;
+        insertCompensation.innerText = data.salary_up_to;
+        insertContactPerson.innerText = data.job_poster;
+        insertLink.innerText = data.job_link;
+        
       }
     });
   });
@@ -30,10 +43,11 @@ function getJobDetails() {
   else if ((/^https:\/\/ph.indeed.com\/*/).test(platform)) {
   return isFromIndeed();
   }
+  else if ((/^https:\/\/www.upwork.com\/*/).test(platform)) {
+  return isFromUpwork();
+  }
 
-
-
-
+  // ##### FROM OLJ CODE BLOCK ##### //
   function isFromOLJ() {
     let jobTitle = document.querySelector('body > section.bg-primary.section-perks.pt-4 > div > div > div > h1')?.innerText || "";
     let hoursPerWeek = document.querySelector('body > section.bg-ltblue.pt-4.pt-lg-0 > div > div.card.job-post.shadow.mb-4.mb-md-0 > div > div > div:nth-child(3) > dl > dd > p')?.innerText || "";
@@ -60,6 +74,8 @@ function getJobDetails() {
     
     return { job_title: jobTitle, hours_per_week: hoursPerWeek, salary_up_to: salaryUpTo, job_poster: jobPoster, job_link: jobLink };
     }
+
+  // ##### FROM INDEED CODE BLOCK ##### //
   function isFromIndeed() {
     let jobTitle = document.querySelector("#jobsearch-ViewjobPaneWrapper > div > div.fastviewjob.jobsearch-ViewJobLayout--embedded.css-1s5gqtr.eu4oa1w0.hydrated > div.jobsearch-JobComponent.css-17riagq.eu4oa1w0 > div.jobsearch-HeaderContainer.css-n78gek.eu4oa1w0 > div > div:nth-child(1) > div.jobsearch-JobInfoHeader-title-container.css-bbq8li.eu4oa1w0 > h2")?.innerText || "";
       function trimJobTitle(rawJobTitle) {
@@ -119,5 +135,35 @@ function getJobDetails() {
     return { job_title: jobTitle, hours_per_week: hoursPerWeek, salary_up_to: salaryUpTo, job_poster: jobPoster, job_link: jobLink };
     }
 
+  // ##### FROM UPWROK CODE BLOCK ##### //
+  function isFromUpwork() {
+    let jobTitle = document.querySelector("#main > div.container > div:nth-child(4) > div > div > div.job-details-card.d-flex.gap-0.air3-card.air3-card-outline.p-0 > div > section:nth-child(1) > h4 > span")?.innerText || "";
+    let completeJobDetails = document.querySelector("#main > div.container > div:nth-child(4) > div > div")?.innerText || "";
+    let salaryUpTo = document.querySelector("body > div.job-details-slider.air3-fullscreen-element > div > div.air3-fullscreen-container.is-scrolled-top.is-scrolled-bottom > div")?.innerText || "";
+    salaryUpTo = salaryUpTo.replace("\n"," ")   
+        function extractSalaryUpTo(jobDetails) {
+          // Constructing the regex with the hardcoded "-price" constant
+          const priceRegex = /\b(\$[\d.]+)\b[^$]*-price/i;
+
+          const priceMatch = jobDetails.match(priceRegex);
+
+          let result = '';
+
+          if (priceMatch) {
+              result += priceMatch[1].trim();
+          }
+          return result;
+          }
+
+    let jobPoster = document.querySelector("#jobsearch-ViewjobPaneWrapper > div > div.fastviewjob.jobsearch-ViewJobLayout--embedded.css-1s5gqtr.eu4oa1w0.hydrated > div.jobsearch-JobComponent.css-17riagq.eu4oa1w0 > div.jobsearch-HeaderContainer.css-n78gek.eu4oa1w0 > div > div:nth-child(1) > div.css-2wyr5j.eu4oa1w0 > div > div > div > div.css-1h46us2.eu4oa1w0 > div.css-hon9z8.eu4oa1w0 > span > a")?.innerText || ""; 
+    let jobLink = window.location.href;
+    salaryUpTo = extractSalaryUpTo(salaryUpTo);
+    console.log(jobTitle);
+    console.log(hoursPerWeek);
+    console.log(salaryUpTo);
+    console.log(jobPoster);
+    console.log(jobLink)
+    
+    return { job_title: jobTitle, hours_per_week: hoursPerWeek, salary_up_to: salaryUpTo, job_poster: jobPoster, job_link: jobLink };
+    }
   }
-  
